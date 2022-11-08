68a8bfde391fec47adb7d2280c142ab22a4cc84b
hatr <hatr@zhaw.ch>
2022-11-08 13:48:12 +0100

    Added gitignore, license and init

---
+++ __init__.py
@@ -0,0 +1,5 @@
+__version__ = "0.1"
+__author__ = ["Thomas Berger"]
+__license__ = "MIT"
+
+from .process_spectra import dummy_fun
---
+++ process_spectra.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+def dummy_fun(x):
+    return x
