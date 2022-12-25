6636ce30e3abf8efbaac176ba289b38d92a1b823
Berger Thomas (bertho01) <bertho01@students.zhaw.ch>
2022-12-25 22:09:33 +0100

    use new files for test with o patcience and small size

--- main_param.py
+++ main_param.py
@@ -17,7 +17,7 @@ spectra_files,file_names = get_data.get_spectra_files(folder_path,delete_experim
 shift_lim = [500, 1800]
 
 cropped_files = [cropper.crop_file(f, shift_lim) for f in spectra_files]
-smooth_files = [smoother.whittaker_smoother(19000, 3) for f in cropped_files]
+smooth_files = [smoother.whittaker_smoother(f,19000, 3) for f in cropped_files]
 
 
 same = ['5','6','11','12','13']
@@ -28,14 +28,14 @@ alleles_smoothing = preprocessparam.get_default_smoothing()
 alleles_baseline = preprocessparam.get_default_baseline()
 alleles_normaliztaion = preprocessparam.get_default_normalization()
 
-pop_size = 30
+pop_size = 15
 mut_prob = 0.5
-patience_cond = 10
+patience_cond = 1
 smoothed_data = smooth_files
 baseline_parameters = alleles_baseline
 same_files = same
 replicants = rep
+file_path = 'C:/Users/thoma/Desktop/Master Thesis/Master/ResultParam'
  
  
- 
-yep = gaparam.mini_ga_param_loop(pop_size,mut_prob,patience_cond,smoothed_data,file_names,baseline_parameters,same_files,replicants,verbose=True):
+yep = gaparam.mini_ga_param_loop(pop_size,mut_prob,patience_cond,smoothed_data,file_names,baseline_parameters,same_files,replicants,file_path,verbose=True)
