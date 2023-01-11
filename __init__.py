f0b920fe4664f062de2b88ab310df51ed9184991
Berger Thomas (bertho01) <bertho01@students.zhaw.ch>
2023-01-11 20:14:34 +0100

    CHangement of the example python file name
    Added the Notebook for the GlucoseCitricAcidAmmoniumChloride in the Yal Medium experiment (visualization /Model creation )

--- __init__.py
+++ __init__.py
@@ -2,4 +2,10 @@ __version__ = "0.1"
 __author__ = ["Thomas Berger"]
 __license__ = "MIT"
 
-from .import dummy_fun
+import get_spectra_data as get_data
+import preprocess_baseline_methods as basecorrecter
+import preprocess_smoothing_methods as smoother
+import preprocess_normalization_methods as normalizer
+import preprocess_cropping_methods as cropper
+import pandas as pd
+import numpy as np
